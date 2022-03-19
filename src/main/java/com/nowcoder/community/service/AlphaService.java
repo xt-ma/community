00bcb9c095093c740d8e3744f88c9263bd50eb0c
xtma <609070078@qq.com>
2022-03-19 14:59:18 +0800

    7.7 热贴排行

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -6,8 +6,12 @@ import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.util.CommunityUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
@@ -36,6 +40,8 @@ public class AlphaService {
 
     @Autowired
     private TransactionTemplate transactionTemplate;
+
+    private static final Logger logger = LoggerFactory.getLogger(AlphaService.class);
 
     public AlphaService() {
 //        System.out.println("实例化AlphaService");
@@ -114,5 +120,16 @@ public class AlphaService {
                 return "ok";
             }
         });
+    }
+
+    // 让该方法在多线程环境下,被异步的调用.
+    @Async
+    public void execute1() {
+        logger.debug("execute1");
+    }
+
+//    @Scheduled(initialDelay = 10000, fixedRate = 1000)
+    public void execute2() {
+        logger.debug("execute2");
     }
 }
